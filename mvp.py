b5ea099efa327babbf3569be0ccaa0dc57d109b8
Katie Hickling <khickling@plos.org>
2017-07-29 16:44:36 -0700

    Added game function and optional exit early in the game

--- mvp.py
+++ mvp.py
@@ -19,7 +19,13 @@ import random
 def welcome (player_name, date_name):
 	print "Welcome to the Haute Maison Restaurant, " + player_name + "! \n"+ "Thank you for bringing your date, " + date_name + " with you.\n"
 	print "You have brought your date, "+ date_name + ", to the restaurant to impress them.\nYou will be served a 3 course meal, selected by Chef JacqueAss. \nYou will select wines to go with your meal.\n"
-	print "However be careful, the Sommelier at the Haute Maison is crazy.\nIf you select a wine that doesn't pair with Chef Jacques food, he may mock you or ask you to leave, and that's not going to impress " + date_name +" is it?\n\n"
+	print "However be careful, the Sommelier at the Haute Maison is crazy.\nIf you select a wine that doesn't pair with Chef Jacques food, he may mock you or ask you to leave, and that's not going to impress " + date_name +" is it?\n"
+	continue_to_table = raw_input("\nWould you like to continue to your table or exit? Type Y to continue: ").capitalize()
+	if continue_to_table == "Y":
+		print "\nWelcome to your table, I am your Sommelier, Ann Garry."
+		return True
+	else:
+		return False
 
 #Show list function
 
@@ -38,9 +44,9 @@ def show_list (wine_list):
 # Wine Selection Function
 
 def wine_selection():
-	wine_choice = raw_input("What is your selection?: ")
+	wine_choice = raw_input("\nWhat is your selection?: ")
 	wine_choice = int(wine_choice)
-	if wine_choice == 0 :
+	if wine_choice == 0:
 		print "\nSommelier says: Get out of my restaurant!\n" 
 		return False
 	elif wine_choice == 1 :
@@ -53,13 +59,44 @@ def wine_selection():
 #Course play function
 
 def course (course_name, course, course_wine_list):
-	print "Your " + course_name + " today will be, " + course
+	print "\nYour " + course_name + " today will be, " + course + "\n"
 	show_list(course_wine_list)
-	wine_selection()
+	good_selection = wine_selection()
+	return good_selection
+
+#Game function
+
+#Notes on Game function:
+
+#For extra fun and chaos a meal is selected at random from the lists and assigned to appetizer, main and dessert variables
+
+#For all of the courses we are calling the course function. The course function calls the show list and wine selection functions, 
+#wine selection not only prints the
+#message to the user about their selection but returns a value, true or false,
+#(this is assigned to a variable "good_selection" for ease of reading but doesn't need to be. 
+#The value is used to break the while loop below.
+#We use a conditional that if the return of the call of the function 'course' is False then the While loop breaks and play ends. 
+
+
+def game():
+
+	appetizer = random.choice(appetizers)
+	main_course = random.choice(mains)
+	dessert = random.choice(desserts)
+
+	#Appetizer
+	if course ("appetizer", appetizer, appetizer_wine_list) == False:
+		return
+	#Main Course
+	if course ("entree", main_course, main_wine_list) == False:
+		return
+	#Dessert
+	if course ("dessert", dessert, dessert_wine_list) == False:
+		return
 
 #------------PYTHON STARTS RUNNING FROM HERE------------
 
-#Lists to be used in game play------
+#Lists to be used in game play-----------------
 
 #meal wine lists
 
@@ -69,11 +106,11 @@ dessert_wine_list = ["Muscadelle", "Sauternes", "Pinot Gris", "Prosecco"]
 
 #meal food lists
 
-appetizers = ["Spinach and Goat upside down cake" , "Lobster-Avocado Cocktail" , "Salmon Rillettes with smoked kelp" , "Quadruple Baked Souffle with Port"]
+appetizers = ["Spinach and goat cheese upside down cake" , "Lobster-Avocado Cocktail" , "Salmon Rillettes with salt-dried kelp" , "Quadruple Baked Souffle with Port Reduction"]
 mains = ["Skate wing with shaved sea ice" , "Pork cheeks in a black tuffle bao" , "Desconstructed Vegan Moussakka" , "Quail and dandelions with fermented pepper"]
-desserts = ["A tea scented gateau with earl grey foam" , "Marscapone tower" , "A test tube of Black Forest Mousse" , "A edible balloon of pineapple smoked air"]
+desserts = ["A tea scented gateau with earl grey foam" , "Marscapone tower" , "A test tube of Black Forest Mousse" , "An edible balloon of pineapple smoked air"]
 
-#End of lists-----------
+#End of lists-----------------------------------
 
 #Playing is contained in a While Loop to allow game to be played again:
 
@@ -83,33 +120,16 @@ while True:
 
 	player_name = raw_input("What is your name? ")
 	date_name = raw_input("What is your date's name?")
-	welcome(player_name, date_name)
+	if welcome(player_name, date_name) == False:
+		break
 
-	#For extra fun and chaos a meal is selected at random from the lists and assigned to app, main and dessert variables used above
-	# making it extra fun if you play the game again
-
-	appetizer = random.choice(appetizers)
-	main_course = random.choice(mains)
-	dessert = random.choice(desserts)
-
-	#Next section at your table welcome and meal variables:
-	welcome_to_your_table = "Welcome to your table, I am your Sommelier, Ann Garry."
-
-	#Play begins!
-
-	#Appetizer
-	print welcome_to_your_table
-	course ("appetizer", appetizer, appetizer_wine_list)
-
-	#Main Course
-	course ("entree", main_course, main_wine_list)
-
-	#Dessert
-	course ("dessert", dessert, dessert_wine_list)
+	#Play begins! Calling the game function
+	
+	game()
 
 	#End of game
 
-	play_again = raw_input("Thanks for playing, to play again enter Y: ").capitalize()
+	play_again = raw_input("THE END\nThanks for playing, to play again enter Y: ").capitalize()
 	if play_again != "Y":
 		break
 
